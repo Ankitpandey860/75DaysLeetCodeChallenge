cba424bc33af5625dcceb25d8214ea2d7b1b208b
Ankitpandey860 <ankitp6040@gmail.com>
2026-04-05 18:33:20 +0530

    Time: 83 ms (16.2%), Space: 55.1 MB (5.8%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -26,10 +26,49 @@ public:
         }
         return dp[i1][i2]=ans;
     }
-    bool isMatch(string s, string p) {
+    bool tab(string& s, string& p){
         int n=s.length();
         int m=p.length();
+        vector<vector<int>>dp(n+2,vector<int>(m+2,false));
+        dp[n][m]=true;
+        for(int i1=n;i1>=0;i1--){
+            for(int i2=m;i2>=0;i2--){
+                if(i1==n&&i2==m) continue;
+                if(i1==s.length()&&p[i2]=='*'){
+                    dp[i1][i2]= dp[i1][i2+1];
+                    continue;
+                }
+                
+                bool ans=false;
+                if(i2<m&&p[i2]=='?'){
+                    ans=dp[i1+1][i2+1];
+                }
+                else if(i2<m&&p[i2]=='*'){
+                    ans=dp[i1+1][i2+1]|dp[i1+1][i2]|dp[i1][i2+1];
+                }
+                else if(i2<m&&i2<m&&p[i2]==s[i1]){
+                    ans=dp[i1+1][i2+1];
+                }
+                
+                dp[i1][i2]=ans;
+            }
+        }
+        return dp[0][0];
+    }
+    bool isMatch(string s, string p) {
+        /*int n=s.length();
+        int m=p.length();
         vector<vector<int>>dp(n+2,vector<int>(m+2,-1));
-        return solve(s,p,0,0,dp);
+        return solve(s,p,0,0,dp);*/
+        if(s.empty()){
+            int i=0;
+            while(i<p.length()){
+                if(p[i]!='*') return false;
+                i++;
+            }
+            
+            return true;
+        }
+        return tab(s,p);
     }
 };
