fb609878dd84173cd3eb099d8d7648be5f022bd3
Ankitpandey860 <ankitp6040@gmail.com>
2026-04-09 01:05:01 +0530

    Time: 11 ms (84.05%), Space: 16.4 MB (71.02%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     bool solve(int s1, int e1, int s2, int e2, string& str1, string& str2,vector<vector<vector<int>>>& dp) {
         if (e1 - s1 != e2 - s2)
             return false;
@@ -29,6 +30,25 @@ public:
                 return dp[s1][s2][len]=true;
         }
         return dp[s1][s2][len]=false;
+    }*/
+    bool solve(int s1,int s2,int len, string& str1, string& str2,vector<vector<vector<int>>>& dp) {
+        
+        if(len==1){
+            return str1[s1]==str2[s2];
+        }
+        if(len<0) return false;
+        
+        //int len=e2-s2+1;
+        if(dp[s1][s2][len]!=-1) return dp[s1][s2][len];
+        for (int i = 1; i < len; i++) {
+            bool llrr = solve(s1, s2,i, str1, str2,dp) &&
+                        solve(s1 + i,s2 + i,len-i, str1, str2,dp);
+            bool lrrl = solve(s1, s2+len-i,i,str1, str2,dp) &&
+                        solve(s1 + i,s2,len-i, str1, str2,dp);
+            if (llrr || lrrl)
+                return dp[s1][s2][len]=true;
+        }
+        return dp[s1][s2][len]=false;
     }
     bool isScramble(string s1, string s2) {
         int n = s1.length();
@@ -43,6 +63,7 @@ public:
             return false;
         
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(n + 1, -1)));
-        return solve(0, n - 1, 0, m - 1, s1, s2,dp);
+        //return solve(0, n - 1, 0, m - 1, s1, s2,dp);
+        return solve(0,0,n,s1,s2,dp);
     }
 };
