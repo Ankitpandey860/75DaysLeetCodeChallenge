feefb0de4970bc0dc113ca75b6d4df4959f8267d
Ankitpandey860 <ankitp6040@gmail.com>
2026-04-15 22:20:07 +0530

    Time: 416 ms (6.5%), Space: 8 MB (90.17%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
-    bool solve(string& s,string& p ,int i,int j,vector<vector<int>>& dp){
-        if(i>=s.length()&&j>=p.length()) return true;
-        if(j>=p.length()) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool currMatch = i < s.length() && (p[j] == '.' || s[i] == p[j]);
-        if(j+1<p.length()&&p[j+1]=='*'){
-            bool exc=solve(s,p,i,j+2,dp);
-            bool inc=currMatch&&solve(s,p,i+1,j,dp);
-            return dp[i][j]=exc||inc;
+    int solve(string& A,string& B,int i,int j){
+        if(i==A.length()&&j==B.length()) return true;
+        if(j==B.length()) return false;
+        if(i==A.length()){
+            if(j+1<B.length()&&B[j+1]=='*')
+                return solve(A,B,i,j+2);
+            return false;
         }
-        else if(currMatch){
-            return dp[i][j]=solve(s,p,i+1,j+1,dp);
+        
+        if(j+1<B.length()&&B[j+1]=='*'){
+            bool skip=false;
+            bool take=false;
+            if(A[i]==B[j]||B[j]=='.'){
+                take=solve(A,B,i+1,j);
+            }
+            skip=solve(A,B,i,j+2);
+            return skip||take;
+        }
+        else if(B[j]=='.'){
+            return solve(A,B,i+1,j+1);
+        }
+        else if(A[i]==B[j]){
+            return solve(A,B,i+1,j+1);
         }
         else{
-            return dp[i][j]=false;
+            return false;
         }
-    } 
+    }
     bool isMatch(string s, string p) {
-        int n=s.length();
-        int m=p.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(s,p,0,0,dp);
+        return solve(s,p,0,0);
     }
 };
