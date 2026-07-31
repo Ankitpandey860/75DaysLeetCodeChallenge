d736e34f41e9a42591e3df4ef238f338deccd86a
Ankitpandey860 <ankitp6040@gmail.com>
2026-07-31 15:10:21 +0530

    Time: 272 ms (32.12%), Space: 72.3 MB (49.74%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        if(source==target) return 0;
         int n=routes.size();
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<routes.size();i++){
+        unordered_map<int,vector<int>>rtb,btr;
+        for(int i=0;i<n;i++){
             for(auto it:routes[i]){
-                mp[it].push_back(i);
+                rtb[i].push_back(it);
+                btr[it].push_back(i);
             }
         }
-        unordered_set<int>vis2;
-        vector<int>vis1(n,false);
-        queue<pair<int,int>>q;
-        q.push({source,0});
-        vis2.insert(source);
-        while(!q.empty()){
-            int curr=q.front().first;
-            int dis=q.front().second;
-            q.pop();
-            if(curr==target) return dis;
-            for(auto it:mp[curr]){
-                if(!vis1[it]){
-                    for(auto it2:routes[it]){
-                        if(vis2.count(it2)==0){
-                            q.push({it2,dis+1});
-                            vis2.insert(it2);
-                        }
-                    }
-                    vis1[it]=true;
+        // step bus
+        using p=tuple<int,int>;
+        priority_queue<p,vector<p>,greater<p>>pq;
+        vector<int>vis(n,false);
+        for(auto it:btr[source]){
+            vis[it]=true;
+            for(auto it2:rtb[it]){
+                pq.push({1,it2});
+            }
+        }
+        while(!pq.empty()){
+            auto [step,bus]=pq.top();
+            pq.pop();
+            if(bus==target) return step;
+            for(auto rots:btr[bus]){
+                if(vis[rots]) continue;
+                vis[rots]=true;
+                for(auto bus2:rtb[rots]){
+                    pq.push({step+1,bus2});
                 }
             }
         }
